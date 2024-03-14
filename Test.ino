946345d3df3d918b05c08e5818d803685d04252f
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-03-14 18:45:59 +0330

    Create Test.ino

---
+++ Test.ino
@@ -0,0 +1,31 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  pinMode(A3, INPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Hello Welcome ;D");
+  delay(1000);
+  lcd.clear();
+  for(int x = 1; x < 11; x++){
+    lcd.print("Download : ");
+    lcd.print(x);
+    delay(1000);
+    lcd.clear();
+  }
+  lcd.print("OPEN #_#");
+
+}
+
+void loop() {
+  digitalWrite(A3, HIGH);
+  lcd.setCursor(0,1);
+  lcd.print("ON ");
+  delay(1000);
+  digitalWrite(A3, LOW);
+  lcd.setCursor(0,1);
+  lcd.print("OFF");
+  delay(1000);
+}
